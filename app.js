f7d61e05842b3a0626f6fb16f9779339686b27a5
akashjaiswar <akashj@snapwork.com>
2025-08-24 08:33:35 +0530

    changes for blue green deployment by usiing eb clone

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var data = {
-  title: 'Hello World!',
+  title: 'Study Sync v2',
   selected: null,
   question: 'In the Battle of Wolf 359 what prominent Miranda-class ship was destroyed?',
   choices: [
